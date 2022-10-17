5fb0c858ba956cc647cabaf618d4a4c8a8967c4b
Jared <jbeckersalax9@aol.com>
2022-10-17 14:43:22 -0400

    commit tree 73fd378f013b3c6e02b45af4299068bcb3ed1226

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 // add any needed imports here
 
 function Users() {
+  const users = useSelector((state)=>state.users);
+  // console.log(users)
+
   return (
     <div>
       <ul>
         Users!
-        {/* Write code here that displays the usernames of all users in the Redux store */}
-        {/* In addition, display the total number of users curently in the store */}
+        { users.map((user) => <li key={user.username}> {user.username} </li>)}
+        { <p>Total Users: {users.length}</p> }
       </ul>
     </div>
   );
